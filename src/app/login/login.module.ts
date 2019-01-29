c5607438c227fabda1a189f1dd4f710810b49a8b
Wagner Parisoto <wagnerparisoto@gmail.com>
2019-01-29 21:17:59 -0200

    remocao fonte

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { BsComponentComponent } from '../layout/bs-component/bs-component.component';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-import { BsComponentModule } from '../layout/bs-component/bs-component.module';
 
 @NgModule({
-  imports: [CommonModule, TranslateModule, LoginRoutingModule, BsComponentModule],
-  declarations: [LoginComponent]
+  imports: [CommonModule, TranslateModule, LoginRoutingModule],
+  declarations: [LoginComponent, BsComponentComponent]
 })
 export class LoginModule {}
